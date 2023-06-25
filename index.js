50a4f8c63c4f16813d70c2788653ea1da5a92317
Your Name <abdullahi.abass@student.moringschool.com>
2023-06-25 15:12:17 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,50 +1,55 @@
-URL = "http://localhost:3000/characters"
+// This URL shows where the anamiamls is fetched
+const URL = "http://localhost:3000/characters";
 
-function fetchAnimals(){
-    fetch(URL)
+// Fetch animals from the URL
+function fetchAnimals() {
+  fetch(URL)
     .then(res => res.json())
-    .then(data => renderAnimals(data))
+    .then(data => renderAnimals(data));
 }
 
-function renderAnimals(data){
-    const ul = document.createElement("animals")
-    const div = document.getElementById("card")
+// Render the animals on the page
+function renderAnimals(data) {
+  const ul = document.getElementById("animals");
+  const div = document.getElementById("card");
 
-    data.forEach(animal => {
-        const li = document.createElement("li")
-        li.innerHTML = animal.name;
+  data.forEach(animal => {
+    const li = document.createElement("li");
+    li.innerHTML = animal.name;
 
-
-        const animalCard = document.createElement("div");
-        animalCard.classList.add("animal-card");
-        animalCard.innerHTML = "
+    const animalCard = document.createElement("div");
+    animalCard.classList.add("animal-card");
+    animalCard.innerHTML = `
         <img src="${animal.image}"/>
         <h2>${animal.name}</h2>
-        
-        ";
-        const votes = document.createElement("div")
-        votes.innerHTML = "Votes: ${animal.votes}";
+    `;
 
-        animalCard.appendChild(votes);
+    const votes = document.createElement("div");
+    votes.innerHTML = `Votes: ${animal.votes}`;
 
-        const btn = document.createElement("button");
-        btn.textContent = "VOTE"
+    animalCard.appendChild(votes);
 
-        btn.addEventListener("click", () => {
-            votes.innerText = "Votes:${parseInt(votes.innerText.split((': ')[1]) +1";
-        })
-        animalCard.appendChild(btn)
+    const btn = document.createElement("button");
+    btn.textContent = "VOTE";
 
-        li.addEventListener("click", () => {
-            div.innerText=""
-            div.appendChild(animalCard);
-            if (!animalCard.classList.contains("active")) {
-                animalCard.classList.add("active");
-                div.appendChild(animalCard);
-            }
-        });
-        ul.appendChild(li);
+    btn.addEventListener("click", () => {
+      votes.innerText = `Votes: ${parseInt(votes.innerText.split(": ")[1]) + 1}`;
     });
 
+    animalCard.appendChild(btn);
+
+    li.addEventListener("click", () => {
+      div.innerText = "";
+      div.appendChild(animalCard);
+      if (!animalCard.classList.contains("active")) {
+        animalCard.classList.add("active");
+        div.appendChild(animalCard);
+      }
+    });
+
+    ul.appendChild(li);
+  });
 }
+
+// Fetch and render the animals
 fetchAnimals();
