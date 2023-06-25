7543cfb61493e3632a85428b743e4eccfc397d5e
Your Name <abdullahi.abass@student.moringschool.com>
2023-06-25 22:45:57 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,55 +1,118 @@
-// This URL shows where the animals are fetched
-const URL = "http://localhost:3000/characters";
+const CHARACTERS_URL = "http://localhost:3000/characters";
+const animalList = document.getElementById("animals-list");
+const animalDetailsContainer = document.getElementById("animal-details");
+const voteButton = document.getElementById("vote-button");
+const resetButton = document.getElementById("reset-button");
+const addAnimalForm = document.getElementById("add-animal-form");
 
 // Fetch animals from the URL
-function fetchAnimals() {
-  fetch(URL)
-    .then((res) => res.json())
-    .then((data) => renderAnimals(data));
+async function fetchAnimals() {
+  try {
+    const response = await fetch(CHARACTERS_URL);
+    const data = await response.json();
+    renderAnimals(data);
+  } catch (error) {
+    console.log("Error fetching animals:", error);
+  }
 }
 
 // Render the animals on the page
 function renderAnimals(data) {
-  const ul = document.getElementById("animals");
-  const div = document.getElementById("card");
-
+  animalList.innerHTML = "";
   data.forEach((animal) => {
     const li = document.createElement("li");
-    li.innerHTML = animal.name;
-
-    const animalCard = document.createElement("div");
-    animalCard.classList.add("animal-card");
-    animalCard.innerHTML = `
-        <img src="${animal.image}"/>
-        <h2>${animal.name}</h2>
-    `;
-
-    const votes = document.createElement("div");
-    votes.innerHTML = `Votes: ${animal.votes}`;
-
-    animalCard.appendChild(votes);
-
-    const btn = document.createElement("button");
-    btn.textContent = "VOTE";
-
-    btn.addEventListener("click", () => {
-      votes.innerText = `Votes: ${parseInt(votes.innerText.split(": ")[1]) + 1}`;
-    });
-
-    animalCard.appendChild(btn);
-
+    li.textContent = animal.name;
     li.addEventListener("click", () => {
-      div.innerText = "";
-      div.appendChild(animalCard);
-      if (!animalCard.classList.contains("active")) {
-        animalCard.classList.add("active");
-        div.appendChild(animalCard);
-      }
+      showAnimalDetails(animal);
     });
-
-    ul.appendChild(li);
+    animalList.appendChild(li);
   });
 }
+
+// Show the details of the selected animal
+function showAnimalDetails(animal) {
+  animalDetailsContainer.innerHTML = `
+    <h2>${animal.name}</h2>
+    <img src="${animal.image}" alt="${animal.name}">
+    <p>Votes: <span id="vote-count">${animal.votes}</span></p>
+  `;
+
+  voteButton.disabled = false;
+  voteButton.addEventListener("click", () => {
+    incrementVotes(animal);
+  });
+}
+
+// Increment the votes for the selected animal
+function incrementVotes(animal) {
+  const voteCountElement = document.getElementById("vote-count");
+  animal.votes += 1;
+  voteCountElement.textContent = animal.votes;
+}
+
+// Reset the votes for all animals
+async function resetVotes() {
+  try {
+    const response = await fetch(CHARACTERS_URL);
+    const data = await response.json();
+    data.forEach((animal) => {
+      animal.votes = 0;
+    });
+
+    await fetch(CHARACTERS_URL, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    // Reset the UI
+    const voteCountElements = document.getElementsByClassName("vote-count");
+    Array.from(voteCountElements).forEach((element) => {
+      element.textContent = "0";
+    });
+  } catch (error) {
+    console.log("Error resetting votes:", error);
+  }
+}
+
+// Add a new animal
+async function addAnimal(event) {
+  event.preventDefault();
+
+  const nameInput = document.getElementById("animal-name");
+  const imageInput = document.getElementById("animal-image");
+
+  const newAnimal = {
+    name: nameInput.value,
+    image: imageInput.value,
+    votes: 0,
+  };
+
+  try {
+    await fetch(CHARACTERS_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newAnimal),
+    });
+
+    // Clear the form inputs
+    nameInput.value = "";
+    imageInput.value = "";
+
+    // Fetch and render the updated animals
+    fetchAnimals();
+  } catch (error) {
+    console.log("Error adding animal:", error);
+  }
+}
+
+// Event listeners
+resetButton.addEventListener("click", resetVotes);
+addAnimalForm.addEventListener("submit", addAnimal);
 
 // Fetch and render the animals
 fetchAnimals();
